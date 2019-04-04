168e8b71eae1c2b19baed278227e8f901873a3df
JohnPaul <testerzttest@gmail.com>
2019-04-04 12:53:21 +0530

    test.php added Signed-off-by: JohnPaul <testerzttest@gmail.com>

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title>Test for GitHub in SalesScripter</title>>
+<title>Test for GitHub in SalesScripter11</title>>
 <body>
 <div>
 <script src="//salesscripter.com/betapro/js/widget.js" type="text/javascript"></script>
